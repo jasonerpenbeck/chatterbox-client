3e47c33a348cd79c939d079cc4a21fb223fc9852
jasonerpenbeck <jason.erpenbeck@gmail.com>
2014-09-22 15:48:36 -0700

    User can enter username and hard-coded message

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,27 +1,47 @@
-// YOUR CODE HERE:
-
-
 var app = {};
 
+app.defaultRoom = 'chatterbox';
 app.server = 'https://api.parse.com/1/classes/chatterbox';
 app.init = function () {};
 app.send = function () {
-  return true;
+  var message = {};
+  message.roomname = this.defaultRoom;
+  message.text = 'sdkgfhskd';
+  message.username = this.username || 'Anonymous';
+
+  $.ajax({
+    url: this.server,
+    type: 'POST',
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message sent');
+      app.fetch();
+    },
+    error: function (data) {
+      // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to send message');
+    }
+  });
 };
 
 
 app.fetch = function () {
   $.ajax({
-    url: this.server,
+    url: this.server + '?order=-createdAt',
     type: 'GET',
     dataType: 'json',
     success: function(data) {
 
+      // Empty out everything in the content div before reloading fetched messages
+      $('#content').empty();
+
       var results = data.results;
       var contentList = $('#content').append('<ul>');
-      console.log(results.length);
 
       for(var i =0; i < results.length; i++) {
+        console.log(results[i].roomname);
+
         var message = $('<li>');
         message.text(results[i].text);
         contentList.append(message);
