d9368f08275af5563ba18fdba631c0c1a6d217c3
jasonerpenbeck <jason.erpenbeck@gmail.com>
2014-09-22 14:43:38 -0700

    Fetch method is working

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,27 @@
 // YOUR CODE HERE:
 
+
+var app = {};
+
+app.server = 'https://api.parse.com/1/classes/chatterbox';
+app.init = function () {};
+app.send = function () {
+  return true;
+};
+
+
+app.fetch = function () {
+  $.ajax({
+    url: this.server,
+    type: 'GET',
+    dataType: 'json',
+    success: function(data) {
+
+    },
+    error: function(xhr, status, errorThrown) {
+      console.log('Sorry, but we could not fetch any messages.');
+    }
+
+  });
+
+};
