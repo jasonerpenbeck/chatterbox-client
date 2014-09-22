c8fa9ecff9d0400817411b27edc553bc533b4483
jasonerpenbeck <jason.erpenbeck@gmail.com>
2014-09-22 15:08:43 -0700

    Messages display on index.html

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -17,6 +17,16 @@ app.fetch = function () {
     dataType: 'json',
     success: function(data) {
 
+      var results = data.results;
+      var contentList = $('#content').append('<ul>');
+
+      for(var i =0; i < results.length; i++) {
+        var message = $('<li>'+decodeURI(results[i].text)+'</li>');
+
+        // escape the message
+        contentList.append(message);
+      }
+
     },
     error: function(xhr, status, errorThrown) {
       console.log('Sorry, but we could not fetch any messages.');
